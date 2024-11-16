c13c6d33f093f776dac68c91c7160c793446865e
Neil Bryan Apostol <neilbryanapostol@gmail.com>
2024-11-17 02:30:08 +0800

    Update http.js

--- src/util/http.js
+++ src/util/http.js
@@ -5,7 +5,7 @@ export async function fetchEvents() {
         const error = new Error('An error occurred while fetching the events');
         error.code = response.status; //added response status to the error
         error.info = await response.json(); //added response info to the error
-        throw error;
+        throw error; //throwing the error
       }
 
       const { events } = await response.json();
