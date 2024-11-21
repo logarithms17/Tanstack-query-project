e218677fe72a4701eda578da10eae7b20b1e84b1
Neil Bryan Apostol <neilbryanapostol@gmail.com>
2024-11-21 19:29:39 +0800

    update newEvent

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,15 +1,21 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query"; //is used to make a request to the server to create a new event
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
+  const { data } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
   function handleSubmit(formData) {}
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
         <>
           <Link to="../" className="button-text">
--- src/util/http.js
+++ src/util/http.js
@@ -1,20 +1,43 @@
-export async function fetchEvents({signal, searchTerm}) {
-  console.log(searchTerm)
-  let url = 'http://localhost:3000/events'
-  
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm);
+  let url = 'http://localhost:3000/events';
+
   if (searchTerm) {
-    url = `http://localhost:3000/events?search=${searchTerm}`
+    url += '?search=' + searchTerm;
   }
-    const response = await fetch(url, {signal});
 
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the events');
-      error.code = response.status; //added response status to the error
-      error.info = await response.json(); //added response info to the error
-      throw error; //throwing the error
-    }
+  const response = await fetch(url, { signal: signal });
 
-    const { events } = await response.json();
-
-    return events;
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the events');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
   }
+
+  const { events } = await response.json();
+
+  return events;
+}
+
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
